8fac52c11c21f9ed81109bd48b1bdb20132fa165
jakehrr <jthartnoll@yahoo.co.uk>
2025-10-09 10:24:30 +0100

    Location UI bars

--- Assets/Scripts/UILookAt.cs
+++ Assets/Scripts/UILookAt.cs
@@ -4,6 +4,11 @@ public class UILookAt : MonoBehaviour
 {
     [SerializeField] private Transform cam;
 
+    private void Awake()
+    {
+        cam = GameObject.Find("CAMERA").GetComponent<Transform>();
+    }
+
     private void LateUpdate()
     {
         Vector3 forward = cam.transform.forward;
